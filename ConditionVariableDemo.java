0f86cb448da29071efcf0d85e2b013539956ad72
Wagner Franchin <wagnerjfr@yahoo.com.br>
2023-07-23 17:02:23 +0100

    Update ConditionVariableDemo.java

--- ConditionVariableDemo.java
+++ ConditionVariableDemo.java
@@ -1,5 +1,3 @@
-package main.concurrency;
-
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -8,41 +6,40 @@ import java.util.concurrent.locks.ReentrantLock;
  * Alternate 3 threads while consuming of a share resource using Condition variable
  */
 
-class Consumer extends Thread {
-
+public class ConditionVariableDemo {
     private static int items = 10;
-    private static Lock lock = new ReentrantLock();
-    private static Condition alternate = lock.newCondition();
+    private static final Lock lock = new ReentrantLock();
+    private static final Condition alternate = lock.newCondition();
 
-    private int id;
+    static class Consumer extends Thread {
+        private final int id;
 
-    Consumer(int id) {
-        this.id = id;
-    }
+        Consumer(int id) {
+            this.id = id;
+        }
 
-    public void run() {
-        while (items > 0) {
-            lock.lock();
-            try {
-                while (items % 3 != id && items > 0) {
-                    alternate.await();
+        public void run() {
+            while (items > 0) {
+                lock.lock();
+                try {
+                    while (items % 3 != id && items > 0) {
+                        alternate.await();
+                    }
+                    if (items > 0) {
+                        System.out.println("Consumer " + id + " took the item " + items);
+                        items--;
+                        alternate.signalAll();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
                 }
-                if (items > 0) {
-                    System.out.println("Consumer " + id + " took the item " + items);
-                    items--;
-                    alternate.signalAll();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                lock.unlock();
             }
         }
     }
-}
 
-public class ConditionVariableDemo {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         for (int i = 0; i <= 2; i++) {
             new Consumer(i).start();
         }
